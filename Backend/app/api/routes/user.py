039e922c64c807a8b4171812d71a28f93e149975
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-24 21:59:47 +0530

    Add user data retrieval and JWT token creation functionality

--- Backend/app/api/routes/user.py
+++ Backend/app/api/routes/user.py
@@ -5,9 +5,18 @@ from app.core.responce import ApiResponse
 from app.schema import userSchema
 from app.database import mongoDb
 from app.utils.hashing import Hash
+from app.utils.JWTtoken import create_access_token
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
+
+load_dotenv()
+
 
 
 router = APIRouter(tags=["User"])
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 @router.post('/create_user',response_model=dict)
@@ -31,7 +40,7 @@ async def create_user(request : userSchema.CreateUser):
     if not user:
         return ApiResponse.error(message="User not created!",status_code=500,errors="Internal server error")
     
-    return ApiResponse.success(message="User created successfully")
+    return ApiResponse.success(message="User created successfully",data=str(user.inserted_id),status_code=201)
 
 
 
@@ -55,7 +64,27 @@ async def login(request : userSchema.login):
     if not check_psw:
         return ApiResponse.error(message="Please enter valid password",status_code=401,errors="Not valid password")
     
+
     user["_id"] = str(user["_id"])
     user.pop("password", None)
 
-    return ApiResponse.success(data=user,message="Login successfully",status_code=200)
+    access_token = create_access_token(
+    data={"sub": user["email"]},
+    expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    return ApiResponse.success(data=access_token,message="Login successfully",status_code=200)
+
+@router.get("/user_data",response_model=dict)
+async def user():
+    collection = mongoDb.get_user_collection()
+    users=collection.find({})
+    users_list=[]
+    for user in users:
+        temp={
+            "_id":str(user["_id"]),
+            "email":user["email"]
+        }
+        users_list.append(temp)
+    return ApiResponse.success(data=users_list,status_code=200,message="user details are fetched")
+    
---
+++ Backend/app/utils/JWTtoken.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+import os
+from jose import jwt,JWTError
+from app.core.responce import ApiResponse
+
+load_dotenv()
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM=os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=60)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verifyJWT(token : str):
+    try:
+        # Decode token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        # Extract user identity (we usually store email or user_id in "sub")
+        user_identity: str = payload.get("sub")
+
+        if user_identity is None:
+            raise ApiResponse.error(message="Unothorized access",status_code=401) 
+
+        return user_identity  # valid token ✅
+
+    except JWTError:
+        raise ApiResponse.error(message="Unothorized access",status_code=401,errors=JWTError) 
