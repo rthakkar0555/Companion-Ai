cc00ad2da462d6afba2ae7684cd19fb7d9257d10
malaysheta <152248884+malaysheta@users.noreply.github.com>
2025-12-26 22:33:32 +0530

    Add endpoint to fetch admin manuals and improve user manual retrieval logic

--- Backend/app/api/routes/manuals.py
+++ Backend/app/api/routes/manuals.py
@@ -13,8 +13,36 @@ async def get_users_manual(curr_user:dict=Depends(get_current_user)):
     if collection is None:
         raise ApiError(message="Manuals collection not found",status_code=500,errors="Internal server error")
     
+
     manuals = []
-    manual = collection.find({"uploaded_by":curr_user},{
+    if curr_user == "admin@gmail.com":
+        manual = collection.find({"uploaded_by": {"$ne": "admin@gmail.com"}},{
+        "_id": 0,
+        "company_name": 1,
+        "product_name": 1,
+        "file_name": 1,
+        "uploaded_by":1})
+    else:
+        manual = collection.find({"uploaded_by":curr_user},{
+        "_id": 0,
+        "company_name": 1,
+        "product_name": 1,
+        "file_name": 1})  
+
+    for x in manual:
+        manuals.append(x)
+    
+    return ApiResponse.success(data=manuals,status_code=200,message="Manuals fetch successfully")
+        
+@router.get('/admins_manulas')
+async def get_admins_manual(curr_user:dict=Depends(get_current_user)):
+    collection = get_manual_collection()
+    
+    if collection is None:
+        raise ApiError(message="Manuals collection not found",status_code=500,errors="Internal server error")
+    
+    manuals = []
+    manual = collection.find({"uploaded_by":"admin@gmail.com"},{
         "_id": 0,
         "company_name": 1,
         "product_name": 1,
@@ -23,5 +51,5 @@ async def get_users_manual(curr_user:dict=Depends(get_current_user)):
     for x in manual:
         manuals.append(x)
     
-    return ApiResponse.success(data=manuals,status_code=200,message="Manuals fetch successfully")
-        
+    return ApiResponse.success(data=manuals,status_code=200,message="Admins Manuals fetch successfully")
+    
