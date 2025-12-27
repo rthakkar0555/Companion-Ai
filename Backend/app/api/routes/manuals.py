820d4a151445798b1a5536fda8a6046aac2788cf
rthakkar0555 <rishithakkar0555@gmail.com>
2025-12-28 03:07:49 +0530

    ✨ feat(manual): delete route
    adding delete route for manuals

--- Backend/app/api/routes/manuals.py
+++ Backend/app/api/routes/manuals.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter,Depends
 from app.database.mongoDb import get_manual_collection
 from app.utils.oauth2 import get_current_user
 from app.core.responce import ApiError,ApiResponse
+from cloudinary.uploader import destroy
+from app.utils.cloudinary_config import cloudinary
+# from cloudinary.api import resource
 
 router = APIRouter(tags=["manuals"])
 
@@ -53,3 +56,41 @@ async def get_admins_manual(curr_user:dict=Depends(get_current_user)):
     
     return ApiResponse.success(data=manuals,status_code=200,message="Admins Manuals fetch successfully")
     
+@router.delete("/manual/{company_name}/{product_name}")
+async def delete_manual(company_name:str,product_name:str,curr_user:dict=Depends(get_current_user)):
+    collection=get_manual_collection()
+    if collection is None:
+        raise ApiError("Manual collection not found",status_code=500,errors="something went wrong on server")
+    manual=collection.find_one({
+        "company_name":company_name,
+        "product_name":product_name
+    },{
+        "cloudinary_file_public_id":1,
+        "cloudinary_qr_public_id":1,
+        "uploaded_by":1,
+    })
+    if manual is None:
+        raise ApiError(status_code=404,message="manual not found")
+    #qdrant embedings deletion code
+    print(manual)
+    if (curr_user != "admin@gmail.com") & (manual["uploaded_by"] != curr_user):
+        raise ApiError(status_code=401,message="unauthorize access, you are not permited to delete this manual")
+    
+    manual_public_id=manual["cloudinary_file_public_id"]
+    manual_qr_public_id=manual["cloudinary_qr_public_id"]
+    #deleting from cloudinary
+    # this code is for fixing and detecting bugs if they comes again in future
+    # print(resource(manual_public_id))
+    # resource_type_manual_pdf=resource(manual_public_id).get("resource_type")
+    # print(resource_type_manual_pdf)
+    res=destroy(public_id=manual_public_id,resource_type="raw")
+    print(res)
+    res=destroy(public_id=manual_qr_public_id,resource_type="image")
+    print(res)
+    print("manual deleted successfully")
+    collection.delete_one({
+        "_id":manual["_id"]
+    })
+
+    return ApiResponse.success(message="collection deleted successfully",data={},status_code=202)
+
